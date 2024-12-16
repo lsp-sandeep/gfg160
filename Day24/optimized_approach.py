aeab4bb2ff13aa68b166c2958f442a3c765f7d06
lakkoju.sandeep <lakkoju.sandeep@werize.com>
2024-12-17 00:01:29 +0530

    day24

---
+++ Day24/optimized_approach.py
@@ -0,0 +1,15 @@
+class Solution:
+	def mergeOverlap(self, arr):
+		#Optimized Approach
+		arr.sort()
+		n = len(arr)
+		res = [arr[0]]
+		for i in range(1, n):
+		    start, end = arr[i][0], arr[i][1]
+
+	        if start <= res[-1][1]:
+	            res[-1][1] = max(res[-1][1], end)
+            else:
+                res.append([start, end])
+        
+        return res
